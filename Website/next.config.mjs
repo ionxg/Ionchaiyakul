d7f5d0ddc67a67a27ccfa94d033853b98747641a
wachirawit chaiyakul <ionxgion@gmail.com>
2026-03-15 15:09:07 +1300

    change file directory
    Changing file directory to direct to github.io

--- Website/next.config.mjs
+++ Website/next.config.mjs
@@ -1,11 +1,15 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig = {
+  output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/Ionchaiyakul" : "",
+  assetPrefix: isProd ? "/Ionchaiyakul/" : "",
 }
 
 export default nextConfig
